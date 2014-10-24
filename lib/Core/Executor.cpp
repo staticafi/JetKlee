e9380dd72670e1577ba1fec50c15e7d750c07f5c
Jiri Slaby <jslaby@suse.cz>
2014-10-24 10:54:41 +0200

    vim-styled output

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -2800,7 +2800,7 @@ void Executor::terminateStateOnError(ExecutionState &state,
   if (EmitAllErrors ||
       emittedErrors.insert(std::make_pair(lastInst, message)).second) {
     if (ii.file != "") {
-      klee_message("ERROR: %s:%d: %s", ii.file.c_str(), ii.line, message.c_str());
+      klee_message("ERROR: %s +%d: %s", ii.file.c_str(), ii.line, message.c_str());
     } else {
       klee_message("ERROR: (location information missing) %s", message.c_str());
     }
