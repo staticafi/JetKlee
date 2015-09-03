6571dff98488417cf20d1c53c46b20f435af98c0
Jiri Slaby <jslaby@suse.cz>
2015-09-03 10:24:49 +0200

    generate better .path files
    We need witnesses for the upcoming SV-COMP. For that purpose we need
    to know the path through the source file. I.e. sequence of code lines
    how to get to an erroneous location. I implemented it though the
    generated .path files. Instead of emitting only a number 0/1 (false
    branch/true branch), I also put there file name and line number.
    
    This implements issue #55.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- lib/Core/Executor.h
+++ lib/Core/Executor.h
@@ -441,6 +441,8 @@ private:
   void printDebugInstructions(ExecutionState &state);
   void doDumpStates();
 
+  std::string getPathInfo(const ExecutionState &state, bool trueBranch);
+
 public:
   Executor(llvm::LLVMContext &ctx, const InterpreterOptions &opts,
       InterpreterHandler *ie);
