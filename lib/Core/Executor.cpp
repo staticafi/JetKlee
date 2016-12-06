8ed7b14cdc493897f05f2e6e0bed6482c41dda49
Marek Chalupa <mchalupa@mail.muni.cz>
2016-12-06 15:54:18 +0100

    Executor: give a message about found error even when it is not in the set of errors we search for
    If we use the -exit-on-error-type switch, it tells us to exit when we
    find the error of given type, but there's no reason to not to give
    a message that tells us, that we found also other errors.
    Since the execution is terminated on any error, user may want to know
    where the execution was terminated and that some parts of the program
    may have not been explored (after the error state).
    
    Signed-off-by: Marek Chalupa <mchalupa@mail.muni.cz>

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3689,12 +3689,10 @@ void Executor::terminateStateOnError(ExecutionState &state,
   if (shouldExitOn(terminationType))
     haltExecution = true;
 
-  // emit the error if we either should emit all errors, or if we search
-  // for a specific error and this is the error (haltExecution is set to true),
-  // or if we do not search for a specific error and we haven't emitted this error yet
-  if (EmitAllErrors || haltExecution ||
-      (ExitOnErrorType.empty() &&
-      emittedErrors.insert(std::make_pair(lastInst, message)).second)) {
+  bool notemitted = emittedErrors.insert(std::make_pair(lastInst, message)).second;
+
+  // give a message about found error
+  if (EmitAllErrors || notemitted) {
     if (!ii.file.empty()) {
       klee_message("ERROR: %s:%d: %s", ii.file.c_str(), ii.line, message.c_str());
     } else {
@@ -3702,7 +3700,12 @@ void Executor::terminateStateOnError(ExecutionState &state,
     }
     if (!EmitAllErrors)
       klee_message("NOTE: now ignoring this error at this location");
+  }
 
+  // process the testcase if we either should emit all errors, or if we search
+  // for a specific error and this is the error (haltExecution is set to true),
+  // or if we do not search for a specific error and we haven't emitted this error yet
+  if (EmitAllErrors || haltExecution || (ExitOnErrorType.empty() && notemitted)) {
     std::string MsgString;
     llvm::raw_string_ostream msg(MsgString);
     msg << "Error: " << message << '\n';
