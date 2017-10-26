a8198a50f9f9facffb40cb4707d915eda16bf7e2
Marek Chalupa <xchalup4@fi.muni.cz>
2017-10-26 17:10:51 +0200

    klee_make_symbolic: number symbolic objects names
    If the program creates more symbolic objects with the same name,
    assign them numbers to tell them apart.

--- lib/Core/ExecutionState.cpp
+++ lib/Core/ExecutionState.cpp
@@ -85,6 +85,7 @@ ExecutionState::~ExecutionState() {
 }
 
 ExecutionState::ExecutionState(const ExecutionState& state):
+    symbolicNames(state.symbolicNames),
     pc(state.pc),
     prevPC(state.prevPC),
     stack(state.stack),
