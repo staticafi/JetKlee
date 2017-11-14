1faddfe0c1ddb2e8a6b9368e928e0864107ec192
Marek Chalupa <xchalup4@fi.muni.cz>
2017-11-14 19:26:33 +0100

    Executor: constrain the size of allocated memory for huge mallocs
    We need the allocated pieces of memory to be dijunctive.
    If we do not constrain the size in path condition, then
    this memory object can by allocated "over" some other memory
    object, which may be problem with our instrumentation that
    supposes that the allocated memory is disjunctive.
    This is only under-approx, though.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3194,6 +3194,9 @@ void Executor::executeAlloc(ExecutionState &state,
           bindLocal(target, *hugeSize.second,
                     ConstantExpr::alloc(1 << 17, Context::get().getPointerWidth()));
 
+          addConstraint(*hugeSize.second,
+                        UleExpr::create(size, ConstantExpr::create(1 << 17, Context::get().getPointerWidth())));
+
           executeAlloc(*fixedSize.second,
                        ConstantExpr::create(1 << 17, Context::get().getPointerWidth()),
                        isLocal, target, zeroMemory, reallocFrom);
