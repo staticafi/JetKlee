f47116ab3e243dcdb3d6ee600a3a46d56c2b9da3
Michael Simacek <msimacek@redhat.com>
2018-02-09 22:56:12 +0100

    Propagate segment into executeFree

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3365,6 +3365,7 @@ void Executor::executeAlloc(ExecutionState &state,
 }
 
 void Executor::executeFree(ExecutionState &state,
+                           ref<Expr> segment,
                            ref<Expr> address,
                            KInstruction *target) {
   StatePair zeroPointer = fork(state, Expr::createIsZero(address), true);
