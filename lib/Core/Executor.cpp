c21bc2bd340a07bdce53c653c980f80e868c1e36
Michael Simacek <msimacek@redhat.com>
2018-02-22 00:34:36 +0100

    Check segments in bounds checks

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3277,7 +3277,7 @@ void Executor::executeMemoryOperation(ExecutionState &state,
     bool inBounds;
     solver->setTimeout(coreSolverTimeout);
     bool success = solver->mustBeTrue(state, 
-                                      mo->getBoundsCheckOffset(offset, bytes),
+                                      mo->getBoundsCheckPointer(address, bytes),
                                       inBounds);
     solver->setTimeout(0);
     if (!success) {
