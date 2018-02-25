0dcc2145b6be3d404af33c5661fcd2362949189a
Michael Simacek <msimacek@redhat.com>
2018-02-26 00:47:26 +0100

    Propagate segment into globalAddresses

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -470,15 +470,14 @@ void Executor::initializeGlobalObject(ExecutionState &state, ObjectState *os,
                              offset + i*elementSize);
   } else if (!isa<UndefValue>(c)) {
     unsigned StoreBits = targetData->getTypeStoreSizeInBits(c->getType());
-    ref<ConstantExpr> C = evalConstant(c);
+    KValue C = evalConstant(c);
 
     // Extend the constant if necessary;
-    assert(StoreBits >= C->getWidth() && "Invalid store size!");
-    if (StoreBits > C->getWidth())
-      C = C->ZExt(StoreBits);
+    assert(StoreBits >= C.getWidth() && "Invalid store size!");
+    if (StoreBits > C.getWidth())
+      C.ZExt(StoreBits);
 
-    // TODO offset
-    os->write(offset, KValue(C));
+    os->write(offset, C);
   }
 }
 
@@ -522,7 +521,7 @@ void Executor::initializeGlobals(ExecutionState &state) {
       legalFunctions.insert((uint64_t) (unsigned long) (void*) f);
     }
     
-    globalAddresses.insert(std::make_pair(f, addr));
+    globalAddresses.insert(std::make_pair(f, KValue(addr)));
   }
 
   // Disabled, we don't want to promote use of live externals.
@@ -600,8 +599,7 @@ void Executor::initializeGlobals(ExecutionState &state) {
                                           /*alignment=*/globalObjectAlignment);
       ObjectState *os = bindObjectInState(state, mo, false);
       globalObjects.insert(std::make_pair(v, mo));
-      // TODO segment?
-      globalAddresses.insert(std::make_pair(v, cast<ConstantExpr>(mo->getPointer().getOffset())));
+      globalAddresses.insert(std::make_pair(v, mo->getPointer()));
 
       // Program already running = object already initialized.  Read
       // concrete value and write it to our copy.
@@ -617,7 +615,6 @@ void Executor::initializeGlobals(ExecutionState &state) {
                      i->getName().data());
 
         for (unsigned offset=0; offset<size; offset++)
-          // TDOO segment
           os->write8(offset, 0, ((unsigned char*)addr)[offset]);
       }
     } else {
@@ -630,8 +627,7 @@ void Executor::initializeGlobals(ExecutionState &state) {
         llvm::report_fatal_error("out of memory");
       ObjectState *os = bindObjectInState(state, mo, false);
       globalObjects.insert(std::make_pair(v, mo));
-      // TODO segment?
-      globalAddresses.insert(std::make_pair(v, cast<ConstantExpr>(mo->getPointer().getOffset())));
+      globalAddresses.insert(std::make_pair(v, mo->getPointer()));
 
       if (!i->hasInitializer())
           os->initializeToRandom();
@@ -1602,7 +1598,7 @@ void Executor::executeInstruction(ExecutionState &state, KInstruction *ki) {
       for (SwitchInst::CaseIt i = si->case_begin(), e = si->case_end(); i != e;
            ++i) {
 #endif
-        ref<Expr> value = evalConstant(i.getCaseValue());
+        ref<Expr> value = evalConstant(i.getCaseValue()).getValue();
 
         BasicBlock *caseSuccessor = i.getCaseSuccessor();
         expressionOrder.insert(std::make_pair(value, caseSuccessor));
@@ -2495,7 +2491,7 @@ void Executor::computeOffsets(KGEPInstruction *kgepi, TypeIt ib, TypeIt ie) {
       Value *operand = ii.getOperand();
       if (Constant *c = dyn_cast<Constant>(operand)) {
         ref<ConstantExpr> index = 
-          evalConstant(c)->SExt(Context::get().getPointerWidth());
+          cast<ConstantExpr>(evalConstant(c).getValue())->SExt(Context::get().getPointerWidth());
         ref<ConstantExpr> addend = 
           index->Mul(ConstantExpr::alloc(elementSize,
                                          Context::get().getPointerWidth()));
@@ -2534,8 +2530,7 @@ void Executor::bindModuleConstants() {
   kmodule->constantTable = new Cell[kmodule->constants.size()];
   for (unsigned i=0; i<kmodule->constants.size(); ++i) {
     Cell &c = kmodule->constantTable[i];
-    c.value = evalConstant(kmodule->constants[i]);
-    c.pointerSegment = ConstantExpr::create(0, c.value->getWidth());
+    c = evalConstant(kmodule->constants[i]);
   }
 }
 
