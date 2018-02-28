fcb3f1aa11a9d3dfba015849f9cc4c63ca23a430
Michael Simacek <msimacek@redhat.com>
2018-02-28 16:41:16 +0100

    Implement ops in KValue

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -609,7 +609,7 @@ void Executor::initializeGlobalObject(ExecutionState &state, ObjectState *os,
     // Extend the constant if necessary;
     assert(StoreBits >= C.getWidth() && "Invalid store size!");
     if (StoreBits > C.getWidth())
-      C.ZExt(StoreBits);
+      C = C.ZExt(StoreBits);
 
     os->write(offset, C);
   }
@@ -2068,7 +2068,7 @@ void Executor::executeArithmeticInstruction(ExecutionState &state, KInstruction 
   //    right.isPointer()
   //);
   // TODO check the constraint
-  KValue result(SelectExpr::create(left.isPointer(),
+  KValue result(SelectExpr::create(Expr::createIsZero(left.getSegment()),
                                    left.getSegment(),
                                    right.getSegment()),
                 exprFn(left.getValue(), right.getValue()));
