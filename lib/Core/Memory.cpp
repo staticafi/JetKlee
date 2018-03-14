84bc7f330c5d8a2858302e6fad75b44b9fee2dbf
Michael Simacek <msimacek@redhat.com>
2018-03-14 23:54:27 +0100

    Remove ObjectState::size field

--- lib/Core/Memory.cpp
+++ lib/Core/Memory.cpp
@@ -567,7 +567,6 @@ ObjectState::ObjectState(const MemoryObject *mo)
   : copyOnWriteOwner(0),
     refCount(0),
     object(mo),
-    size(mo->size),
     readOnly(false),
     segmentPlane(0),
     offsetPlane(new ObjectStatePlane(this)){
@@ -579,7 +578,6 @@ ObjectState::ObjectState(const MemoryObject *mo, const Array *array)
   : copyOnWriteOwner(0),
     refCount(0),
     object(mo),
-    size(mo->size),
     readOnly(false),
     segmentPlane(0),
     offsetPlane(new ObjectStatePlane(this, array)) {
@@ -590,7 +588,6 @@ ObjectState::ObjectState(const ObjectState &os)
   : copyOnWriteOwner(0),
     refCount(0),
     object(os.object),
-    size(os.size),
     readOnly(false),
     segmentPlane(0),
     offsetPlane(new ObjectStatePlane(this, *os.offsetPlane)) {
