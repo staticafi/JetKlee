2609c53d024ec9eebb47f5a2f0b0d04e484f5811
Marek Chalupa <xchalup4@fi.muni.cz>
2018-04-03 12:46:43 +0200

    port klee main.cpp to LLVM 4.0

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -31,11 +31,16 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/Support/Errno.h"
 #include "llvm/Support/FileSystem.h"
-#include "llvm/Bitcode/ReaderWriter.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/raw_ostream.h"
+
+#if LLVM_VERSION_MAJOR < 4
+#include "llvm/Bitcode/ReaderWriter.h"
+#else
+#include "llvm/Bitcode/BitcodeReader.h"
+#endif
 
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/Signals.h"
