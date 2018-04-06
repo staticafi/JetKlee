83ffaa267defac5c7b9bc59ba3836fc0ba12bf95
Marek Chalupa <xchalup4@fi.muni.cz>
2018-04-06 18:25:23 +0200

    StatsTracker: do not crash on memory leak
    It tried to used iterator of an instructions "after main".
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- lib/Core/StatsTracker.cpp
+++ lib/Core/StatsTracker.cpp
@@ -481,6 +481,8 @@ void StatsTracker::updateStateStatistics(uint64_t addend) {
   for (std::set<ExecutionState*>::iterator it = executor.states.begin(),
          ie = executor.states.end(); it != ie; ++it) {
     ExecutionState &state = **it;
+    if (!state.pc)
+        continue;
     const InstructionInfo &ii = *state.pc->info;
     theStatisticManager->incrementIndexedValue(stats::states, ii.id, addend);
     if (UseCallPaths)
@@ -918,6 +920,9 @@ void StatsTracker::computeReachableUncovered() {
 
       if (next==es->stack.end()) {
         kii = es->pc;
+        // no other instruction to execute
+        if (!kii)
+            break;
       } else {
         kii = next->caller;
         ++kii;
