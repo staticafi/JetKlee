b9f93d3c597ecefc55062b2f7c565c607cb486dd
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-12 21:16:14 +0100

    Allow allocating memory on 32-bit addresses
    This is to avoid:
    
    static klee::ref<klee::ConstantExpr>
    klee::ConstantExpr::create(uint64_t, klee::Expr::Width):
    Assertion `v == bits64::truncateToNBits(v, w) && "invalid constant"' failed.
    
    When running 32-bit bitcode on 64-bit machines.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -361,6 +361,7 @@ Executor::Executor(LLVMContext &ctx, const InterpreterOptions &opts,
       interpreterHandler->getOutputFilename(SOLVER_QUERIES_KQUERY_FILE_NAME));
 
   this->solver = new TimingSolver(solver, EqualitySubstitution);
+
   memory = new MemoryManager(&arrayCache);
 
   initializeSearchOptions();
@@ -449,6 +450,7 @@ Executor::setModule(std::vector<std::unique_ptr<llvm::Module>> &modules,
   DataLayout *TD = kmodule->targetData.get();
   Context::initialize(TD->isLittleEndian(),
                       (Expr::Width)TD->getPointerSizeInBits());
+  memory->useLowMemory(TD->getPointerSizeInBits() == 32);
 
   return kmodule->module.get();
 }
@@ -3733,7 +3735,7 @@ void Executor::runFunctionAsMain(Function *f,
 
   // hack to clear memory objects
   delete memory;
-  memory = new MemoryManager(NULL);
+  memory = new MemoryManager(nullptr, NumPtrBytes * 8);
 
   globalObjects.clear();
   globalAddresses.clear();
