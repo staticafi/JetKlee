5dc661502955770f59ce2deccc67f7781698706f
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-19 14:37:29 +0100

    KModule: add mapping Instruction -> KInstruction
    Sometimes we need to get KInstruction for Instruction.

--- include/klee/Internal/Module/KModule.h
+++ include/klee/Internal/Module/KModule.h
@@ -48,6 +48,7 @@ namespace klee {
     KInstruction **instructions;
 
     std::map<llvm::BasicBlock*, unsigned> basicBlockEntry;
+    std::map<llvm::Instruction*,KInstruction*> instructionsMap;
 
     /// Whether instructions in this function should count as
     /// "coverable" for statistics and search heuristics.
@@ -61,6 +62,12 @@ namespace klee {
     ~KFunction();
 
     unsigned getArgRegister(unsigned index) { return index; }
+
+    KInstruction *getKInstruction(llvm::Instruction *I) const {
+        auto it = instructionsMap.find(I);
+        assert(it != instructionsMap.end());
+        return it->second;
+    }
   };
 
 
@@ -143,6 +150,8 @@ namespace klee {
 
     /// Return an id for the given constant, creating a new one if necessary.
     unsigned getConstantID(llvm::Constant *c, KInstruction* ki);
+
+    KInstruction *getKInstruction(llvm::Instruction *I) const;
   };
 } // End klee namespace
 
--- lib/Module/KModule.cpp
+++ lib/Module/KModule.cpp
@@ -442,6 +442,7 @@ KFunction::KFunction(llvm::Function *_function,
       Instruction *inst = &*it;
       ki->inst = inst;
       ki->dest = registerMap[inst];
+      instructionsMap[inst] = ki;
 
       if (isa<CallInst>(it) || isa<InvokeInst>(it)) {
         CallSite cs(inst);
@@ -471,4 +472,10 @@ KFunction::~KFunction() {
   for (unsigned i=0; i<numInstructions; ++i)
     delete instructions[i];
   delete[] instructions;
+}
+
+KInstruction *KModule::getKInstruction(llvm::Instruction *I) const {
+    auto it = functionMap.find(I->getParent()->getParent());
+    assert(it != functionMap.end());
+    return it->second->getKInstruction(I);
 }
