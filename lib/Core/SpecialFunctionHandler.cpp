bd7b05647ef98cc686fbeb1c2a2e786d24900caa
Marek Chalupa <mchqwerty@gmail.com>
2021-10-26 15:14:10 +0200

    Fix handling __VERIFIER_nondet_short
    We created the symbol as "__VERIFIER_nondet_ushort".
    Found by @ayazip.

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -1256,7 +1256,7 @@ void SpecialFunctionHandler::handleVerifierNondetShort(ExecutionState &state,
   assert(arguments.empty() && "Wrong number of arguments");
 
   handleVerifierNondetType(state, target, Expr::Int16,
-                           /* isSigned = */ true, "__VERIFIER_nondet_ushort");
+                           /* isSigned = */ true, "__VERIFIER_nondet_short");
 }
 
 void SpecialFunctionHandler::handleVerifierNondetSizeT(ExecutionState &state,
