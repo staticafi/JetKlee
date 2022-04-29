61e38afecce077f10977772292113b59f64c5faf
capso <jakubnovak97@gmail.com>
2022-04-29 17:33:44 +0200

    add lazy-init symbolic values to nondet values
    This makes them show up in ktest files

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -4455,6 +4455,7 @@ Executor::handleReadForLazyInit(ExecutionState &state, KInstruction *target,
       const Array* array = CreateArrayWithName(state, typeWidth, "lazy_init_arr");
       result = Expr::createTempRead(array, typeWidth);
       state.addressSpace.getWriteable(mo,os)->write(offset, result);
+      state.addNondetValue(result, true, "lazy_init_value");
     } else {
       // simple path, value already exists, read it traditionally
       shouldReadFromOffset = true;
@@ -4736,8 +4737,10 @@ void Executor::initializeEntryFunctionArguments(Function *f,
       (void)bindObjectInState(state, mo, false);
       bindArgument(kf, index, state, {mo->getSegmentExpr(), constantZero});
     } else {
-      ref<Expr> value = createTempReadForType(state, ty);
-      bindArgument(kf, index, state, {constantZero, value});
+      ref<Expr> symArray = createTempReadForType(state, ty);
+      KValue value = {constantZero, symArray};
+      bindArgument(kf, index, state, value);
+      state.addNondetValue(value, true, "symbolic_function_arg");
     }
   }
   if (f->isVarArg()) {
