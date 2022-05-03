8e45b64f880ac4e7bd533ff750ee0d4e857ed193
capso <jakubnovak97@gmail.com>
2022-05-03 20:38:35 +0200

    added an option to ignore out of bounds error
    also remove unnecessary comment

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -180,6 +180,13 @@ cl::opt<uint64_t>
                        cl::init(0),
                        cl::cat(SolvingCat));
 
+cl::opt<bool>
+    IgnoreLazyOOB("ignore-lazy-oob",
+                       cl::desc("Ignore out of bounds error on lazy-init "
+                                "objects and don't generate test"),
+                       cl::init(false),
+                       cl::cat(SolvingCat));
+
 
 /*** External call policy options ***/
 
@@ -4295,9 +4302,10 @@ void Executor::executeMemoryOperation(ExecutionState &state,
   
   // XXX there is some query wasteage here. who cares?
   ExecutionState *unbound = &state;
+  const MemoryObject* mo = nullptr;
   
   for (ResolutionList::iterator i = rl.begin(), ie = rl.end(); i != ie; ++i) {
-    const MemoryObject *mo = i->first;
+    mo = i->first;
     const ObjectState *os = i->second;
     ref<Expr> inBounds = mo->getBoundsCheckPointer(optimAddress, bytes);
     
@@ -4344,8 +4352,12 @@ void Executor::executeMemoryOperation(ExecutionState &state,
     if (incomplete) {
       terminateStateEarly(*unbound, "Query timed out (resolve).");
     } else {
-      terminateStateOnError(*unbound, "memory error: out of bound pointer", Ptr,
-                            NULL, getKValueInfo(*unbound, optimAddress));
+      if (IgnoreLazyOOB && mo && mo->isLazyInitialized) {
+        terminateState(*unbound);
+      } else {
+        terminateStateOnError(*unbound, "memory error: out of bound pointer",
+                              Ptr, NULL, getKValueInfo(*unbound, optimAddress));
+      }
     }
   }
 }
