5e69b50b8462b039b611e9e79177c6e7f1b99803
Ema Jasekova <524716@mail.muni.cz>
2024-02-23 00:50:43 +0100

    More memory recording (AddressSpace)

--- lib/Support/ProgressRecorder.cpp
+++ lib/Support/ProgressRecorder.cpp
@@ -44,7 +44,49 @@ namespace klee {
     return expr;
   }
 
-  static void plane2json(std::ostream& ostr, const ObjectStatePlane *const plane) {
+  static void stack2json(std::ostream& ostr, const std::vector<StackFrame> stack){
+    ostr << "\"stack\": [";
+    for (std::size_t i = 0U; i != stack.size(); ++i)
+      if (stack.at(i).callPathNode->callSite != nullptr) {
+        const InstructionInfo *const callerInfo{ stack.at(i).caller->info };
+        ostr << "[\"" << callerInfo->file << "\","
+             << callerInfo->line << ","
+             << callerInfo->column << ","
+             << callerInfo->assemblyLine << "]"
+             << (i + 1U < stack.size() ? "," : "");
+      }
+    ostr << "],\n";
+  }
+
+  static void constraints2json(std::ostream& ostr, const ConstraintSet constraints) {
+    ostr << "    \"constraints\": [\n";
+    for (auto it = constraints.begin(); it != constraints.end(); ++it)
+      ostr << "      \"" << expr2str(*it) << "\"" << (std::next(it) != constraints.end() ? ",\n" : "\n");
+    ostr << "    ]";
+  }
+
+  static void memoryObjs2json(std::ostream& ostr, const MemoryMap objects){
+    ostr << "    \"objects\": [\n";
+    for (auto it = objects.begin(); it != objects.end(); ) {
+      ostr << "      { ";
+      ostr << "\"objID\": " << it->first->id << ", ";
+      ostr << "\"segment\": " << it->first->segment << ", ";
+      ostr << "\"name\": \"" << it->first->name << "\", ";
+      ostr << "\"size\": \"" << expr2str(it->first->size) << "\", ";
+      ostr << "\"isLocal\": " << it->first->isLocal << ", ";
+      ostr << "\"isGlobal\": " << it->first->isGlobal << ", ";
+      ostr << "\"isFixed\": " << it->first->isFixed << ", ";
+      ostr << "\"isUserSpecified\": " << it->first->isUserSpecified << ", ";
+      ostr << "\"isLazy\": " << it->first->isLazyInitialized << ", ";
+      ostr << "\"symAddress\": \"" << (it->first->symbolicAddress ? expr2str(*it->first->symbolicAddress) : "") << "\" ";
+      ostr << "}";
+      ++it;
+      ostr << (it != objects.end() ? ",\n" : "\n");
+    }
+    ostr << "    ],\n";
+  }
+
+    static void plane2json(std::ostream& ostr, const ObjectStatePlane *const plane) {
     if (plane == nullptr) return;
 
     int id = plane->getParent() ? plane->getParent()->getObject()->id : -1;
@@ -65,17 +107,17 @@ namespace klee {
       ostr << "\"unflushed\": " << plane->isByteUnflushed(i) << ", ";
       ostr << "\"value\": " << "\"" << expr2str(plane->read8(i)) << "\"";
       ostr << "}" << (i + 1U < plane->sizeBound ? ",\n" : "\n");
-  }
-  ostr << "      ],\n";
+    }
+    ostr << "      ],\n";
 
-  ostr << "      \"updates\": [\n";
-  for (const auto *un = plane->getUpdateList().head.get(); un; ) {
-    ostr << "        {";
-    ostr << "\"" << expr2str(un->index) << "\"" << " : " << "\"" << expr2str(un->value) << "\"" << "\n";
-    un = un->next.get();
-    ostr << "        }" << (un != nullptr ? ",\n" : "\n");
-  }
-  ostr << "      ]\n";
+    ostr << "      \"updates\": [\n";
+    for (const auto *un = plane->getUpdateList().head.get(); un; ) {
+      ostr << "        {";
+      ostr << "\"" << expr2str(un->index) << "\"" << " : " << "\"" << expr2str(un->value) << "\"" << "\n";
+      un = un->next.get();
+      ostr << "        }" << (un != nullptr ? ",\n" : "\n");
+    }
+    ostr << "      ]\n";
   }
 
   ProgressRecorder& ProgressRecorder::instance() {
@@ -184,46 +226,29 @@ namespace klee {
          << instrInfo->line << ","
          << instrInfo->column << ","
          << instrInfo->assemblyLine << "], ";
+    const InstructionInfo *const nextInstrInfo{ node->state->pc->info };
+    ostr << "\"nextLocation\": ["
+         << "\"" << nextInstrInfo->file << "\","
+         << nextInstrInfo->line << ","
+         << nextInstrInfo->column << ","
+         << nextInstrInfo->assemblyLine << "], ";
+    ostr << "\"incomingBBIndex\": " << node->state->incomingBBIndex << ", ";
     ostr << "\"depth\": " << node->state->depth << ", ";
     ostr << "\"coveredNew\": " << node->state->coveredNew << ", ";
     ostr << "\"forkDisabled\": " << node->state->forkDisabled << ", ";
-    ostr << "\"stack\": [";
-    for (std::size_t i = 0U; i != node->state->stack.size(); ++i)
-      if (node->state->stack.at(i).callPathNode->callSite != nullptr) {
-        const InstructionInfo *const callerInfo{ node->state->stack.at(i).caller->info };
-        ostr << "[\"" << callerInfo->file << "\","
-             << callerInfo->line << ","
-             << callerInfo->column << ","
-             << callerInfo->assemblyLine << "]"
-             << (i + 1U < node->state->stack.size() ? "," : "");
-      }
-    ostr << "],\n";
-    ostr << "    \"constraints\": [\n";
-    for (auto it = node->state->constraints.begin(); it != node->state->constraints.end(); ++it)
-      ostr << "      \"" << expr2str(*it) << "\"" << (std::next(it) != node->state->constraints.end() ? ",\n" : "\n");
-    ostr << "    ]";
+    ostr << "\"instsSinceCovNew\": " << node->state->instsSinceCovNew << ", ";
+    ostr << "\"nextID\": " << node->state->nextID << ", ";
+
+    stack2json(ostr, node->state->stack);
+    constraints2json(ostr, node->state->constraints);
+    
     if (!uniqueState) {
       ostr << "\n";
       return;
     }
     ostr << ",\n";
-    ostr << "    \"objects\": [\n";
-    for (auto it = node->state->addressSpace.objects.begin(); it != node->state->addressSpace.objects.end(); ) {
-      ostr << "      { ";
-      ostr << "\"objID\": " << it->first->id << ", ";
-      ostr << "\"segment\": " << it->first->segment << ", ";
-      ostr << "\"name\": \"" << it->first->name << "\", ";
-      ostr << "\"size\": \"" << expr2str(it->first->size) << "\", ";
-      ostr << "\"isLocal\": " << it->first->isLocal << ", ";
-      ostr << "\"isGlobal\": " << it->first->isGlobal << ", ";
-      ostr << "\"isFixed\": " << it->first->isFixed << ", ";
-      ostr << "\"isLazy\": " << it->first->isLazyInitialized << ", ";
-      ostr << "\"symAddress\": \"" << (it->first->symbolicAddress ? expr2str(*it->first->symbolicAddress) : "") << "\" ";
-      ostr << "}";
-      ++it;
-      ostr << (it != node->state->addressSpace.objects.end() ? ",\n" : "\n");
-    }
-    ostr << "    ],\n";
+
+    memoryObjs2json(ostr, node->state->addressSpace.objects);
     ostr << "    \"objectStates\": [\n";
     for (auto it = node->state->addressSpace.objects.begin(); it != node->state->addressSpace.objects.end(); ) {
       ostr << "      {\n";
@@ -239,6 +264,80 @@ namespace klee {
       ostr << "\n      }";
       ++it;
       ostr << (it != node->state->addressSpace.objects.end() ? ",\n" : "\n");
+    }
+    ostr << "    ],\n";
+
+    ostr << "    \"concreteAddressMap\": [\n";
+    for (auto it = node->state->addressSpace.concreteAddressMap.begin(); it != node->state->addressSpace.concreteAddressMap.end(); ) {
+      ostr << "    {";
+      ostr << "      \"address\": " << it->first << ", ";
+      ostr << "\"segment\": " << it->second;
+      ostr << "}";
+      ++it;
+      ostr << (it != node->state->addressSpace.concreteAddressMap.end() ? ",\n" : "\n");
+    }
+    ostr << "    ], \n";
+
+    ostr << "    \"removedObjectsMap\": [\n";
+    for (auto it = node->state->addressSpace.removedObjectsMap.begin(); it != node->state->addressSpace.removedObjectsMap.end(); ) {
+      ostr << "      {\n";
+      ostr << "      \"segment\": " << it->first << "\", ";
+      ostr << "\"symbolicArray\": \"" << expr2str(it->second) << "\"";
+      ostr << "\n      }";
+      ++it;
+      ostr << (it != node->state->addressSpace.removedObjectsMap.end() ? ", " : "");
+    }
+    ostr << "    ], \n";
+
+    ostr << "    \"lazyObjectsMap\": [\n";
+    for (auto it = node->state->addressSpace.lazyObjectsMap.begin(); it != node->state->addressSpace.lazyObjectsMap.end(); ) {
+      ostr << "      {\n";
+      ostr << "      \"pointerSegment\": " << it->first << "\", ";
+      ostr << "\"offsets\": [\n";
+      for (auto a = it->second.begin(); a != it->second.end(); ) {
+        ostr << expr2str(*a);
+        ++a;
+        ostr << (a != it->second.end() ? ", " : "");
+      }
+      ostr << "    ], \n";
+      ostr << "\n      }";
+      ++it;
+      ostr << (it != node->state->addressSpace.lazyObjectsMap.end() ? ", " : "");
+    }
+    ostr << "    ], \n";
+
+    ostr << "    \"nondetValues\": [\n";
+    for (auto it = node->state->nondetValues.begin(); it != node->state->nondetValues.end(); ) {
+      ostr << "      {\n";
+      ostr << "        \"value\": \"" << expr2str(it->value.getValue()) << "\", ";
+      ostr << "\"offset\": \"" << expr2str(it->value.getOffset()) << "\", ";
+      ostr << "\"segment\": \"" << expr2str(it->value.getSegment()) << "\", ";
+      ostr << "\"isSigned\": \"" << it->isSigned << "\", ";
+      ostr << "\"name\": \"" << it->name << "\"";
+      ostr << "\n      }";
+      ++it;
+      ostr << (it != node->state->nondetValues.end() ? ",\n" : "\n");
+    }
+    ostr << "    ], \n";
+
+    // ostr << "    \"symbolics\": [\n";
+    // for (auto it = node->state->symbolics.begin(); it != node->state->symbolics.end(); ) {
+    //   ostr << "\"objID\": " << it->first->id << ", ";
+    //   for (auto a = it->second.begin(); a != it->second.end(); ) {
+    //     ostr << expr2str(*a);
+    //     ++a;
+    //     ostr << (a != it->second.end() ? ", " : "");
+    //   }
+    //   ++it;
+    //   ostr << (it != node->state->symbolics.end() ? ",\n" : "\n");
+    // }
+    // ostr << "    ], \n";
+
+    ostr << "    \"arrayNames\": [\n";
+    for (auto it = node->state->arrayNames.begin(); it != node->state->arrayNames.end(); ) {
+      ostr << "      \"" << *it << "\"";
+      ++it;
+      ostr << (it != node->state->arrayNames.end() ? ", " : "");
     }
     ostr << "    ]\n";
   }
